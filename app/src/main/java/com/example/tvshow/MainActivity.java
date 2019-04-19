679b73db98d6b677e544731f1cbb7e327ee8f856
Willy Hamzah <wav364@gmail.com>
2019-04-20 07:07:49 +0800

    Tugas Kurus

--- app/src/main/java/com/example/tvshow/MainActivity.java
+++ app/src/main/java/com/example/tvshow/MainActivity.java
@@ -8,6 +8,8 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.tvshow.data.adapter.TvAdapter;
 import com.example.tvshow.data.model.TvAiringToday;
@@ -20,12 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding activityMainBinding;
     private TvAdapter adapter;
+    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        progressBar = findViewById(R.id.prograss);
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
         activityMainBinding.rvTv.setLayoutManager(gridLayoutManager);
@@ -33,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         TvService.getAPI().tvAiringTodayCall("119377682a1e98f078b0484aa494acb1").enqueue(new Callback<TvAiringToday>() {
             @Override
             public void onResponse(Call<TvAiringToday> call, Response<TvAiringToday> response) {
+                progressBar.setVisibility(View.GONE);
                 if (response.isSuccessful()){
                     List<TvAiringToday.ResultsTvshow> resultsTvshows = response.body().getResults();
                     adapter = new TvAdapter(resultsTvshows, MainActivity.this);
@@ -42,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<TvAiringToday> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
 
             }
         });
